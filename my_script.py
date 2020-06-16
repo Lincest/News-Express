2304377dffc7877957176a2ca39223346ffe3dc4
Lincest <474617032@qq.com>
2020-06-16 12:37:41 +0800

    sc4

--- my_script.py
+++ my_script.py
@@ -1,5 +1,13 @@
 import sys
 import os
-msg = "hello world\n"
-file = open("123.txt","w")
-file.write(msg)
+
+with open('./123.txt', 'r') as f:
+    text = f.read()
+
+
+with open('./123.txt', 'w') as f:
+	if(text=="hello world\n"):
+		f.write("not new")
+	else :
+		f.write("I'm new")
+
